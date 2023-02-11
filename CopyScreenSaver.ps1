db133b65f3c07bf09dc431fa2645cd190386dc88
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-11 18:46:47 +0530

    Update CopyScreenSaver.ps1

--- CopyScreenSaver.ps1
+++ CopyScreenSaver.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest 'https://github.com/murtazapdaeservices/pdaeservices/blob/Test/PNQ_Wallpaper.jpg?raw=true' -OutFile c:\Images\Wallpaper\Wallpaper.jpg
+Invoke-WebRequest 'https://github.com/murtazapdaeservices/pdaeservices/blob/Test/PNQ_Wallpaper.jpg?raw=true' -OutFile c:\Wallpaper.jpg
