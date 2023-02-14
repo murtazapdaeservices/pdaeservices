6ab43ba6d9414f84b9add18c91bc0cfe7ac8e5c2
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 19:29:22 +0530

    Update CreateScreenSaverKey.ps1

--- CreateScreenSaverKey.ps1
+++ CreateScreenSaverKey.ps1
@@ -1,1 +1,19 @@
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name SCRNSAVE.EXE -Value "C:\Windows\system32\PNQScreensaver.scr" -PropertyType String
+$ErrorActionPreference= 'silentlycontinue'
+ 
+$SSFile = "${Env:SystemRoot}\PNQScreensaver.scr.scr"
+ 
+Remove-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "ScreenSaveActive"
+ 
+Remove-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "ScreenSaverIsSecure"
+ 
+Remove-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "ScreenSaveTimeOut"
+ 
+Remove-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "SCRNSAVE.EXE"
+ 
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1 -PropertyType String
+ 
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaverIsSecure -Value 1 -PropertyType String
+ 
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 60 -PropertyType String
+ 
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name SCRNSAVE.EXE -Value $SSFile -PropertyType String
