ae78ea772277aefb11b0a7e8f81842d0b8602511
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 20:39:11 +0530

    Update ScreenSaver-2.ps1

--- ScreenSaver-2.ps1
+++ ScreenSaver-2.ps1
@@ -1,1 +1,1 @@
-reg add “HKEY_CURRENT_USER\Control Panel\Desktop” /v SCRNSAVE.EXE /t REG_SZ /d C:\Windows\system32\PNQScreensaver.scr /f
+reg delete “HKEY_CURRENT_USER\Control Panel\Desktop” /v SCRNSAVE.EXE /f
