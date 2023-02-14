5b1b890cb7e82377c17f436ad4e31a892b4d7195
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 18:36:23 +0530

    Update SetScreenSaver.ps1

--- SetScreenSaver.ps1
+++ SetScreenSaver.ps1
@@ -1,3 +1,3 @@
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 15
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name scrnsave.exe -Value "C:\PNQ Screensaver.scr"
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 10
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name scrnsave.exe -Value "c:\windows\system32\PNQScreensaver.scr"
