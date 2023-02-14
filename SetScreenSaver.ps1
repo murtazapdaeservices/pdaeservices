b4595c112ad38182a2d5f575773e893b27ea49eb
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 19:58:42 +0530

    Delete SetScreenSaver.ps1

--- SetScreenSaver.ps1
+++
@@ -1,3 +0,0 @@
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 1
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 10
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name scrnsave.exe -Value "c:\windows\system32\PNQScreensaver.scr"
