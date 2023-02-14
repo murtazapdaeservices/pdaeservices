bc6bca57d81ee3433c5974f3e7908361e4f5acd1
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 18:47:00 +0530

    Update CopyScreenSaver_Locally.ps1

--- CopyScreenSaver_Locally.ps1
+++ CopyScreenSaver_Locally.ps1
@@ -1,5 +1,5 @@
 $url = "https://github.com/murtazapdaeservices/pdaeservices/raw/main/PNQ%20Screensaver.scr"
 
-$output = "C:\PNQ Screensaver.scr"
+$output = "C:\PNQScreensaver.scr"
 
 Invoke-WebRequest -Uri $url -OutFile $output
