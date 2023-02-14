f7682e8ce056e7411eb322eccdbda77518210304
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 18:52:55 +0530

    Update CombineScript.ps1

--- CombineScript.ps1
+++ CombineScript.ps1
@@ -8,3 +8,6 @@ Invoke-WebRequest -Uri $url -OutFile $output
 Copy-Item -Path C:\PNQScreensaver.scr -Destination c:\Windows\System32\
 
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v SCRNSAVE.EXE /t REG_SZ /d C:\Windows\system32\PNQ.scr /f
+
+
+shutdown /r -t 5
