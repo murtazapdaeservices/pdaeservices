ad7e5231b6fdda67eaf8173a7d2d58da80b6ead5
Murtaza Shikora <124716374+murtazapdaeservices@users.noreply.github.com>
2023-02-14 20:21:14 +0530

    Create ScreenSaverPNQ.ps1

---
+++ ScreenSaverPNQ.ps1
@@ -0,0 +1,15 @@
+$url = "https://github.com/murtazapdaeservices/pdaeservices/raw/main/PNQ%20Screensaver.scr"
+
+$output = "C:\PNQScreensaver.scr"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+Copy-Item -Path C:\PNQScreensaver.scr -Destination c:\Windows\System32\
+
+
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaverIsSecure -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 180
+
+reg add “HKEY_CURRENT_USER\Control Panel\Desktop” /v SCRNSAVE.EXE /t REG_SZ /d C:\Windows\system32\PNQScreensaver.scr /f
