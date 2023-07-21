685b6d9bfc59d8b3d30ffaf0a393bb0b2c97a7d3
Murtaza Shikora <124716374+murtazashikora@users.noreply.github.com>
2023-07-21 14:44:58 +0530

    Add files via upload

---
+++ Autlogin_DDS.ps1
@@ -0,0 +1,8 @@
+$Username = 'DDS'
+$Pass = 'Aries2025!'
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
+Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$Username" -type String 
+Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$Pass" -type String
+
+Restart-Computer -Force
