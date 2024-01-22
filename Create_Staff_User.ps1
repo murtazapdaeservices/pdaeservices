9a5cea608aa5668a803c23eec9246eae2c98c775
Murtaza Shikora <124716374+murtazashikora@users.noreply.github.com>
2024-01-23 03:52:06 +0530

    Add files via upload

---
+++ Create_Staff_User.ps1
@@ -0,0 +1,7 @@
+﻿net user /add Staff Teeth731!
+
+net localgroup "Remote Desktop Users" Staff /add
+
+WMIC USERACCOUNT WHERE “Name='Staff'” SET PasswordExpires=FALSE
+
+Net user Staff /Passwordchg:No
